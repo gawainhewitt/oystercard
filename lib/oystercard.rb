826fbb450ab2fbd84472e0a7286b87ba0708df32
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-04-25 16:19:17 +0100

    added balance top up functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,4 +3,7 @@ class OysterCard
   def initialize
     @balance = 0
   end 
+  def top_up(money)
+    @balance += money
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,5 +4,11 @@ describe OysterCard do
   it 'can display initial balance of 0' do
     expect(subject.balance).to eq (0)
   end
+
+  it 'can be topped up' do
+    subject.top_up(35)
+    subject.top_up(12)
+    expect(subject.balance).to eq (47)
+  end
   
 end

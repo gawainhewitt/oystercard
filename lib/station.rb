bd21f27020c899d738e6f91e0e6ac43aeeaff93f
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-04-28 17:21:12 +0100

    changed array for hash for step 13

--- lib/station.rb
+++ lib/station.rb
@@ -2,10 +2,7 @@ class Station
   attr_reader :details
 
   def initialize(station_name, zone)
-    # @station_name = station_name
-    # @zone = zone
-    @details = [station_name, zone]
+    @details = {station_name: station_name, zone: zone}
   end 
-
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -3,7 +3,7 @@ require 'station'
 describe Station do
   it "should know what zone it's in" do
     station = Station.new(:Maida_Vale, 2)
-    expect(station.details).to eq [:Maida_Vale, 2]
+    expect(station.details).to eq({station_name: :Maida_Vale, zone: 2})
   end
 
 end
